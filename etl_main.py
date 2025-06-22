21c1680c37459d11b563993e1526fe6462753174
Kalpesh Shinde <kalpeshtheofficial@gmail.com>
2025-06-22 19:11:39 +0530

    created two new columns 'state' and 'city' based on column 'state-province'

--- etl_main.py
+++ etl_main.py
@@ -28,6 +28,7 @@ print(">> df.head()", "\n", df.head())
 print(">> df.shape", "\n", df.shape)
 print(">> df.info()", "\n", df.info())
 
+
 print("Unique entries from 'country' column:", "\n", df['country'].unique())
 # It is confirmed that the data only consists of 'country': 'India'. Hence removing the country column since it makes no value addition.
 print("Columns before dropping 'country' column: ", "\n", df.columns.to_list())
@@ -40,3 +41,45 @@ print("Unique entries from 'alpha_two_code' column:", "\n", df['alpha_two_code']
 print("Columns before dropping 'alpha_two_code' column: ", "\n", df.columns.to_list())
 df = df.drop(columns='alpha_two_code')
 print("Columns after dropping 'alpha_two_code' column: ", "\n", df.columns.to_list())
+
+column_check_state_province = df['state-province'].value_counts()
+
+# print(type(column_check_state_province), "\n", column_check_state_province)
+
+z = df[df['state-province']=='Mumbai']
+
+print(z)
+
+indian_states = ["Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chhattisgarh", "Goa", "Gujarat", "Haryana", 
+                 "Himachal Pradesh", "Jharkhand", "Karnataka", "Kerala", "Madhya Pradesh", "Maharashtra", "Manipur", 
+                 "Meghalaya", "Mizoram", "Nagaland", "Odisha", "Punjab", "Rajasthan", "Sikkim", "Tamil Nadu", "Telangana", 
+                 "Tripura", "Uttar Pradesh", "Uttarakhand", "West Bengal"]
+
+# print(">> check check", df['state-province'].to_list())
+
+state = []
+city = []
+
+for i in df['state-province'].to_list():
+    # print(i)
+    if i in indian_states:
+        print(f"{i} is a state!")
+        # df['state'] = i
+        state.append(i)
+        city.append("NA")
+    else:
+        print(f"{i} is a city!")
+        # df['city'] = i
+        state.append("NA")
+        city.append(i)
+        
+df['state'] = state
+df['city'] = city
+
+print(">> df.head(15)", "\n", df.head(15))
+
+# As of now, worked on 'state-province' column to separate 'state' and 'city' columns.
+# More operations can be performed on this part. Time being moving ahead.
+
+
+
