c126e2a7601954d21d699705d66d4ac21c6bacab
Kalpesh Shinde <kalpeshtheofficial@gmail.com>
2025-06-22 17:10:11 +0530

    removed Columns: 'country' and 'alpha_two_code' since they were having single value. Additionally, created a new text file called additional_notes for future developments

--- etl_main.py
+++ etl_main.py
@@ -17,9 +17,26 @@ import pandas as pd
 try:
     API_URL = "http://universities.hipolabs.com/search?country=India"
     data = requests.get(API_URL).json()
-    print(">> API_URL", type(API_URL), API_URL)
-    print(">> data", type(data), data)
+    # print(">> API_URL", type(API_URL), API_URL)
+    # print(">> data", type(data), data)
     
 except:
     print("Error occured while fetching the API url! ")
 
+df = pd.DataFrame(data)
+print(">> df.head()", "\n", df.head())
+print(">> df.shape", "\n", df.shape)
+print(">> df.info()", "\n", df.info())
+
+print("Unique entries from 'country' column:", "\n", df['country'].unique())
+# It is confirmed that the data only consists of 'country': 'India'. Hence removing the country column since it makes no value addition.
+print("Columns before dropping 'country' column: ", "\n", df.columns.to_list())
+df = df.drop(columns='country')
+print("Columns after dropping 'country' column: ", "\n", df.columns.to_list())
+
+
+print("Unique entries from 'alpha_two_code' column:", "\n", df['alpha_two_code'].unique())
+# It is confirmed that the data only consists of 'alpha_two_code': 'IN'. Hence removing the country column since it makes no value addition.
+print("Columns before dropping 'alpha_two_code' column: ", "\n", df.columns.to_list())
+df = df.drop(columns='alpha_two_code')
+print("Columns after dropping 'alpha_two_code' column: ", "\n", df.columns.to_list())
