802f4723c6579a6ae42173b77d41ab5455bdcea7
Kalpesh Shinde <kalpeshtheofficial@gmail.com>
2025-06-23 22:17:58 +0530

    done with Transformation

--- etl_main.py
+++ etl_main.py
@@ -48,7 +48,7 @@ column_check_state_province = df['state-province'].value_counts()
 
 z = df[df['state-province']=='Mumbai']
 
-print(z)
+# print(z)
 
 indian_states = ["Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chhattisgarh", "Goa", "Gujarat", "Haryana", 
                  "Himachal Pradesh", "Jharkhand", "Karnataka", "Kerala", "Madhya Pradesh", "Maharashtra", "Manipur", 
@@ -63,12 +63,12 @@ city = []
 for i in df['state-province'].to_list():
     # print(i)
     if i in indian_states:
-        print(f"{i} is a state!")
+        # print(f"{i} is a state!")
         # df['state'] = i
         state.append(i)
         city.append("NA")
     else:
-        print(f"{i} is a city!")
+        # print(f"{i} is a city!")
         # df['city'] = i
         state.append("NA")
         city.append(i)
@@ -76,10 +76,18 @@ for i in df['state-province'].to_list():
 df['state'] = state
 df['city'] = city
 
-print(">> df.head(15)", "\n", df.head(15))
+# print(">> df.head(15)", "\n", df.head(15))
 
 # As of now, worked on 'state-province' column to separate 'state' and 'city' columns.
 # More operations can be performed on this part. Time being moving ahead.
+
+# df['domains'] = df['domains'].astype(str).str.strip('[]')
+# df['web_pages'] = df['web_pages'].astype(str).str.strip('[]')
+
+df['domains'] = df['domains'].astype(str).str.replace('[', '', regex=False).str.replace(']', '', regex=False).str.replace("'", '', regex=False)
+df['web_pages'] = df['web_pages'].astype(str).str.replace('[', '', regex=False).str.replace(']', '', regex=False).str.replace("'", '', regex=False)
+
+print(">> df.head(15)", "\n", df.head(15))
 
 
 
